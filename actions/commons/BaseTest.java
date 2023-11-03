45788fdd5956e4bf2623664d77488592d339fc34
ntaphuong <anhphuong192@gmail.com>
2023-11-03 17:03:15 +0700

    update basePage

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,4 +1,26 @@
 package commons;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.time.Duration;
+
 public class BaseTest {
+    private WebDriver driver;
+    private String projectPath = System.getProperty("user.dir");
+    protected void getBrowserDriver(String browserName){
+        if(browserName.equalsIgnoreCase("firefox")){
+           // System.setProperty("webdriver.gecko.driver", projectPath+"\\browserDrivers\\geckodriver.exe");
+            driver = new FirefoxDriver();
+        }else if(browserName.equalsIgnoreCase("chrome")){
+            driver = new FirefoxDriver();
+        } else if(browserName.equalsIgnoreCase("edge")){
+            driver = new EdgeDriver();
+        } else {
+            throw new RuntimeException("BrowserName is Not Valid.");
+        }
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.get("");
+    }
 }
